4f762c4dacec23944c4d9fb4376e6b8b8d772ed0
Mohsin I Amin <mohsinamin953@gmail.com>
2024-01-02 23:01:59 +0600

    .next dir

---
+++ client/.next/static/chunks/app/home/page-7dc3430408ed164a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[951],{91572:function(n,e,u){Promise.resolve().then(u.bind(u,80029))}},function(n){n.O(0,[580,574,396,29,971,472,744],function(){return n(n.s=91572)}),_N_E=n.O()}]);
